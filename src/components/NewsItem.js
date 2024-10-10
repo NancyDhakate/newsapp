486583e419ef5457e9b6c8c4c66244315cb0023d
NancyDhakate <143214811+NancyDhakate@users.noreply.github.com>
2024-10-10 20:53:01 +0530

    different news

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,7 +6,15 @@ export class NewsItem extends Component {
     return (
       <div className="my-3">
         <div className="card" style={{ width: "18rem" }}>
-          <img src={imageUrl} className="card-img-top" alt="..." />
+          <img
+            src={
+              !imageUrl
+                ? "https://fortune.com/img-assets/wp-content/uploads/2024/10/GettyImages-2154270210-e1728469071550.jpg?resize=1200,600"
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
           <div className="card-body">
             <h5 className="card-title">{title}...</h5>
             <p className="card-text">{description}...</p>

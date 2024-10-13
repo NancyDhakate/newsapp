5d0a7dd2a8ffa48bafd499e0a28934d6d12cf38f
NancyDhakate <143214811+NancyDhakate@users.noreply.github.com>
2024-10-13 23:49:22 +0530

    adding time author

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ export class Navbar extends Component {
   render() {
     return (
       <div>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary navbar-light bg-dark">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <div className="container-fluid">
             <Link className="navbar-brand" to="/">
               NewsMonkey
@@ -31,7 +31,6 @@ export class Navbar extends Component {
                     Home
                   </Link>
                 </li>
-
                 <li className="nav-item">
                   <Link className="nav-link" to="/business">
                     Business
